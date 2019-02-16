25c5e878160c43eff578239a7b58863cd27856c8
Daniel Truong <ddtruong06@gmail.com>
2019-02-16 09:18:07 -0800

    tapped buttons & aligned to ground plane

---
+++ mARk/Assets/Questions/Scripts/BlockPressed.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPressed : MonoBehaviour
+{
+    string btnTag;
+    void Start()
+    {
+        Debug.Log("created");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Debug.Log("touch");
+            // Debug.Log(Camera.main.transform.position + "/" + this.gameObject.transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            
+            RaycastHit hit; 
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Debug.Log("Work dang it");
+                btnTag = hit.transform.tag;
+                Debug.Log(btnTag);
+                switch (btnTag)
+                {
+                    case "QuestionMark":
+                        Debug.Log("Tapping on: " + hit.transform.name);
+                        break;
+                    }
+            }
+        }
+    }
+}
